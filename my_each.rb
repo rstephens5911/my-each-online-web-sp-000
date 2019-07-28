64ec2d5126b8b44575e6a86ae53a6469e8d3eb4c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 16:46:40 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+
+tas = ['arel', 'jon', 'logan', 'spencer']
+def my_each(tas)
+  i = 0
+
+  while i < tas.length
+    yield tas[i]
+    i = i + 1
+  end
+  tas
 end
+
+  my_each(tas) do |word|
+    return word
+  end
