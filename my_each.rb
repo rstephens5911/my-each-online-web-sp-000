93196ebbfd142cfeac077fa3857fc2d40b14ba5f
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-28 16:11:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+  if bolck_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
+  else 
+    puts "no array given."
+  end
+
+  my_each(array) do |name|
+    puts name
+  end
 end
